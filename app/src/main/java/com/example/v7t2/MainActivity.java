b10ad270e95ca54d583b81c12ad05745ee7d6a97
Juha <juha1lahteinen@gmail.com>
2020-05-04 20:15:45 +0300

    Viikko 7 tehtävä 2

---
+++ app/src/main/java/com/example/v7t2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.v7t2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+    }
+
+    public void testFunction(View v) {
+        text.setText("Hello World!");
+    }
+}
